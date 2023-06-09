2e33fdd80a635edcc17211a8a1d9c0ba72dc775f
duongddat <duongvandat080602@gmail.com>
2023-06-09 10:21:28 +0700

    Sort middleware

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import morgan from 'morgan';
 import { engine } from 'express-handlebars';
 import methodOverride from 'method-override';
 
+import SortMiddleware from './app/middlewares/SortMiddleware.js';
 import route from './routes/index.js';
 import db from './config/db/index.js';
 
@@ -22,6 +23,9 @@ app.use(express.json());
 //Overrides methods in express
 app.use(methodOverride('_method'));
 
+//Custom middlewares
+app.use(SortMiddleware);
+
 //HTTP logger
 app.use(morgan('combined'));
 
@@ -33,6 +37,27 @@ app.engine(
         extname: '.hbs',
         helpers: {
             sum: (a, b) => a + b,
+            sortable: (field, sort) => {
+                const sortType = field === sort.column ? sort.type : 'default';
+
+                const icons = {
+                    default: 'oi oi-elevator',
+                    asc: 'oi oi-sort-ascending',
+                    desc: 'oi oi-sort-descending',
+                };
+                const types = {
+                    default: 'desc',
+                    asc: 'desc',
+                    desc: 'asc',
+                };
+
+                const icon = icons[sortType];
+                const type = types[sortType];
+
+                return `<a href="?_sort&column=${field}&type=${type}">
+                    <span class="${icon}"></span>
+                </a>`;
+            },
         },
     }),
 );
